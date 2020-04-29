5b2b09b7692a53c6dff2ce06771d7b8eae034ae3
Eduard Sergeev <eduard.sergeev@gmail.com>
2020-04-29 21:24:53 +1000

    Avoid TypeError in `path.normalizePath`
    Was throwing: 'The "path" argument must be of type string. Received type undefined'

--- src/path.ts
+++ src/path.ts
@@ -3,6 +3,6 @@ import { basename, normalize, resolve } from 'path';
 
 export { basename as fileName };
 
-export function normalizePath(root: string, path: string) { 
-  return normalize(resolve(root, path));
+export function normalizePath(root: string, path: string) {
+  return normalize(root && path ? resolve(root, path) : path);
 }
