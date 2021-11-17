11b4275d07fa4f7da9aff594d751ede4e3987f5f
Eduard Sergeev <eduard.sergeev@gmail.com>
2021-11-18 06:03:09 +1100

    Run tests for specified VSCode version
    Via `CODE_VERSION`

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,6 +1,11 @@
 import * as path from 'path';
+import { env } from 'process';
 
-import { runTests } from 'vscode-test';
+import {
+  runTests,
+  downloadAndUnzipVSCode,
+  resolveCliPathFromVSCodeExecutablePath
+} from 'vscode-test';
 
 async function main() {
   try {
@@ -11,12 +16,21 @@ async function main() {
     // The path to the extension test runner script
     // Passed to --extensionTestsPath
     const extensionTestsPath = __dirname;
+    const vscodeVersion = env['CODE_VERSION'];
+    const vscodeExecutablePath = await downloadAndUnzipVSCode(vscodeVersion);
+    const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
+
+    const extensionsDir = path.resolve(path.dirname(cliPath), '..', 'extensions');
+    const userDataDir = path.resolve(extensionsDir, '..', '..', 'udd');
 
     // Download VS Code, unzip it and run the integration test
     await runTests({
+      vscodeExecutablePath,
       extensionDevelopmentPath,
       extensionTestsPath,
       launchArgs: [
+        '--user-data-dir', userDataDir,
+        '--extensions-dir', extensionsDir,
         '--new-window',
         '--disable-extensions',
         '--disable-gpu',
@@ -31,7 +45,7 @@ async function main() {
   } catch (err) {
     console.error(err);
     console.error('Failed to run tests');
-    process.exit(1);
+    return 1;
   }
 }
 
